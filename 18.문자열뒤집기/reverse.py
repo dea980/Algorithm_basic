84c1b18a4f69f186145580e0e2da31df4943b2df
Daeyeop Kim <kdea989@gmail.com>
2024-12-09 20:36:57 +0900

    Create reverse.py

---
+++ 18.문자열뒤집기/reverse.py
@@ -0,0 +1,5 @@
+# 문자열 my_string이 매개변수로 주어집니다. my_string을 거꾸로 뒤집은 문자열을 return하도록 solution 함수를 완성해주세요.
+def solution(my_string):
+    answer = my_string[::-1]
+    #answer = ''.join.reversed(my_string) => 이경우 list 를 사용
+    return answer
