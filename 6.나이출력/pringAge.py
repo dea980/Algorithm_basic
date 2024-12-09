fe0c7f9a04f574958fe0e7603034c091251fb323
Daeyeop Kim <kdea989@gmail.com>
2024-12-09 20:23:25 +0900

    Create pringAge.py

---
+++ 6.나이출력/pringAge.py
@@ -0,0 +1,5 @@
+# 머쓱이는 선생님이 몇 년도에 태어났는지 궁금해졌습니다. 2022년 기준 선생님의 나이 age가 주어질 때, 선생님의 출생 연도를 return 하는 solution 함수를 완성해주세요
+def solution(age):
+    ## since it start from 1 it needs to add 1 on 2022
+    answer = 2023 - age
+    return answer
